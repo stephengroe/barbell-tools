c9086f388eb98e0fb498be485e22034cfd50ca1b
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-08-08 13:41:11 -0700

    feat: Support weights outside of known increments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Standard divisions of plates and bar weight in pounds
-let plateWeights = [45, 25, 10, 5, 2.5];
+let increments = [45, 25, 10, 5, 2.5];
 let barWeight = 45;
 
 // Get DOM elements
@@ -17,17 +17,25 @@ calculateButton.addEventListener('click', function (e) {
 
 // Generate array with plates necessary
 function calcPlates(weight){
-    let platesNeeded = Array(plateWeights.length).fill(0);
+    let platesNeeded = Array(increments.length).fill(0);
     let remainingWeight = weight;
 
-    for (let key in plateWeights) {
-        let activePlate = plateWeights[key];
-        
+    for (let key in increments) {
+        let activePlate = increments[key];
+
         if (remainingWeight / activePlate < 1) {continue;}
 
         platesNeeded[key] = Math.floor(remainingWeight / activePlate);
         remainingWeight %= activePlate;
     }
+
+    // Include additional weight not included in known increments
+    // Round to two decimals to fix floating point issues
+    if (remainingWeight !== 0){
+        remainingWeight = Math.round(remainingWeight * 100) / 100;
+        platesNeeded.push(remainingWeight);
+    }
+
     return platesNeeded;
 }
 
@@ -41,8 +49,12 @@ function displayPlates(platesNeeded) {
 
         let plateContainer = document.createElement("div");
         plateContainer.classList.add("plate-container");
-        plateContainer.setAttribute("id", plateWeights[key]);
-        plateContainer.textContent = platesNeeded[key] + " \u00D7 " + plateWeights[key];
+        plateContainer.setAttribute("id", increments[key]);
+
+        // Include custom remainder if not known increment
+        plateContainer.textContent = (increments[key])
+            ? platesNeeded[key] + " \u00D7 " + increments[key]
+            : platesNeeded[key] + "lbs.";
 
         for (i = 0; i <= platesNeeded[key]; i++) {
             plateContainer.appendChild(document.createElement("div"));
