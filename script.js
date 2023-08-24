338194fe891eb217f3ee8ebc85af1b0bc9fad736
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-08-23 20:13:53 -0700

    Refactor display module

--- script.js
+++ script.js
@@ -1,57 +1,79 @@
-// Calculator module
-const calculator = (() => {
-    let _output = "";
-    let _calcDisplay = document.querySelector("#calc-display");
-    let _visualizer = document.querySelector("#visualizer");
-    let _calcButton = document.querySelectorAll(".keypad button, .display button");
+// Display module
 
-    function _renderDisplay(_displayValue){
+const display = {
+    _output: 0,
+
+    initDisplay: function(_initValue) {
+        this._output = _initValue;
+        _renderDisplay(_initValue);
+        _bindKeypad();
+    },
+
+    _renderDisplay: function(_displayValue) {
+        let _calcDisplay = document.querySelector("#calc-display");
         _calcDisplay.textContent = _displayValue;
         const _unit = document.createElement("span");
         _unit.setAttribute("id", "unit");
         _unit.textContent = "lbs.";
         _calcDisplay.appendChild(_unit);
+    },
+
+    setDisplay: function(output){
+        _output = output;
+        this._renderDisplay(_output);
     }
 
-    function outputInit(_initValue){
-        _output = _initValue.toString();
-        _renderDisplay(_initValue);
-    }
+    
 
-    function _bindKeypad(){
+
+}
+
+
+
+
+
+
+// Calculator module
+const calculator = {
+
+    _bindKeypad(): {
+        let _calcButton = document.querySelectorAll(".keypad button, .display button");
         _calcButton.forEach(item => {
             item.addEventListener("click", function(){
                 updateOutput(this.dataset.buttonFunction, this.dataset.buttonNumber)
             })
         });
-    }
+    },
 
-    function updateOutput(_function, _number){
+    updateOutput(_function, _number): {
+        console.log(_function, _number);
+        _renderDisplay(_validateOutput(_output));
+    },
 
-        _renderDisplay(_validateOutput(output));
-    }
-
-    function _validateOutput(_number){
-        let output = _number;
+    _validateOutput(_number): {
         return output;
     }
             
     return {
-        outputInit,
+        initCalculator,
         updateOutput,
     };
-})();
+};
+
+calculator.initCalculator(380);
+
 
 // Plate visualizer module
-const plateVisualizer = (() => {
+const plateVisualizer = () => {
+    let _visualizer = document.querySelector("#visualizer");
     let _increments = [45, 25, 10, 5, 2.5];
     let _barWeight = 45;
     let _maxWeight = 1500;
 
-    calculator.outputInit(380);
+    calculator.initCalculator(380);
 
 
-})();
+};
 
 
 /******************
@@ -64,140 +86,140 @@ const plateVisualizer = (() => {
 *******************/
 
 
-// Calculator functions
-calcButton.forEach(item => {
-    item.addEventListener("click", function(){
-        const buttonFunction = this.dataset.buttonFunction;
-        const buttonNumber = this.dataset.buttonNumber;
-        let updateWeight = enteredWeight.toString();
+// // Calculator functions
+// calcButton.forEach(item => {
+//     item.addEventListener("click", function(){
+//         const buttonFunction = this.dataset.buttonFunction;
+//         const buttonNumber = this.dataset.buttonNumber;
+//         let updateWeight = enteredWeight.toString();
 
-        switch (buttonFunction){
-            case "delete":
-                updateWeight = updateWeight.slice(0, -1);
-                break;
-            case "clear":
-                updateWeight = "0";
-                break;
-            case "bump":
-                console.log(buttonNumber);
-                updateWeight = +updateWeight + +buttonNumber;
-                updateWeight = updateWeight.toString();
-                break;
-            case "key":
-                if (updateWeight === "0") updateWeight = buttonNumber;
-                else updateWeight += buttonNumber;
-        }
+//         switch (buttonFunction){
+//             case "delete":
+//                 updateWeight = updateWeight.slice(0, -1);
+//                 break;
+//             case "clear":
+//                 updateWeight = "0";
+//                 break;
+//             case "bump":
+//                 console.log(buttonNumber);
+//                 updateWeight = +updateWeight + +buttonNumber;
+//                 updateWeight = updateWeight.toString();
+//                 break;
+//             case "key":
+//                 if (updateWeight === "0") updateWeight = buttonNumber;
+//                 else updateWeight += buttonNumber;
+//         }
 
-        updateWeight = +updateWeight;
-        if (updateWeight === "" || updateWeight <= 0) {
-            enteredWeight = 0;
-        } else if (updateWeight >= maxWeight ) {
-            calcDisplay.classList.add("error");
-            setTimeout(() => {
-                calcDisplay.classList.remove("error");
-              }, "150");
-            return;
-        } else {
-            enteredWeight = updateWeight;
-        }
+//         updateWeight = +updateWeight;
+//         if (updateWeight === "" || updateWeight <= 0) {
+//             enteredWeight = 0;
+//         } else if (updateWeight >= maxWeight ) {
+//             calcDisplay.classList.add("error");
+//             setTimeout(() => {
+//                 calcDisplay.classList.remove("error");
+//               }, "150");
+//             return;
+//         } else {
+//             enteredWeight = updateWeight;
+//         }
 
-        updateCalcDisplay(enteredWeight);
-        updateVisualizer(enteredWeight);
-    });
-});
+//         updateCalcDisplay(enteredWeight);
+//         updateVisualizer(enteredWeight);
+//     });
+// });
 
-function removeChildNodes(element){
-    while (element.firstChild){
-        element.removeChild(element.firstChild);
-    }
-}
+// function removeChildNodes(element){
+//     while (element.firstChild){
+//         element.removeChild(element.firstChild);
+//     }
+// }
 
 
 /* Plate visualizer functions */
 
 // Set up display at start
-updateCalcDisplay(enteredWeight);
-updateVisualizer(enteredWeight);
+// updateCalcDisplay(enteredWeight);
+// updateVisualizer(enteredWeight);
 
 
-function updateVisualizer(weight) {
-    removeChildNodes(visualizer);
-    if (weight <= barWeight) return;
-    const platesNeeded = calculatePlates(weight);
+// function updateVisualizer(weight) {
+//     removeChildNodes(visualizer);
+//     if (weight <= barWeight) return;
+//     const platesNeeded = calculatePlates(weight);
 
-    for (let increment of platesNeeded){
-        if (increment[1] === 0) {continue;}
+//     for (let increment of platesNeeded){
+//         if (increment[1] === 0) {continue;}
 
-        // Create container
-        let incrementContainer = document.createElement("div");
-        let plateIconClass = "plate-" + increment[0].toString().replace(".","-");
-        incrementContainer.classList.add("incrementContainer", plateIconClass);
+//         // Create container
+//         let incrementContainer = document.createElement("div");
+//         let plateIconClass = "plate-" + increment[0].toString().replace(".","-");
+//         incrementContainer.classList.add("incrementContainer", plateIconClass);
 
-        // Create text
-        let description = incrementContainer.appendChild(document.createElement("h3"));
-        description.textContent = increment[1] + " \u00D7 " + increment[0];
+//         // Create text
+//         let description = incrementContainer.appendChild(document.createElement("h3"));
+//         description.textContent = increment[1] + " \u00D7 " + increment[0];
 
-        // Create plates container
-        let plateContainer = incrementContainer.appendChild(document.createElement("div"));
-        plateContainer.classList.add("plateContainer");
+//         // Create plates container
+//         let plateContainer = incrementContainer.appendChild(document.createElement("div"));
+//         plateContainer.classList.add("plateContainer");
 
-        // Create each plate
-        for (i=0; i<increment[1]; i++){
-            plateContainer.appendChild(document.createElement("div"));
-        }
+//         // Create each plate
+//         for (i=0; i<increment[1]; i++){
+//             plateContainer.appendChild(document.createElement("div"));
+//         }
 
-        visualizer.appendChild(incrementContainer);
-    }
-}
+//         visualizer.appendChild(incrementContainer);
+//     }
+// }
 
-// Generate nested array with plates necessary
-function calculatePlates(weight){
-        weight = (weight - barWeight) / 2; // Reduce to just one side of bar
-        let platesNeeded = increments.map((increment) => {
-            const plates = Math.floor(weight / increment);
-            weight %= increment;
-            return [increment, plates];
-        });
+// // Generate nested array with plates necessary
+// function calculatePlates(weight){
+//         weight = (weight - barWeight) / 2; // Reduce to just one side of bar
+//         let platesNeeded = increments.map((increment) => {
+//             const plates = Math.floor(weight / increment);
+//             weight %= increment;
+//             return [increment, plates];
+//         });
     
-        // Add custom weight increment as a decimal
-        if (weight > 0) platesNeeded.push(["custom", Math.round(weight * 100) / 100]);
-        return platesNeeded;
-}
+//         // Add custom weight increment as a decimal
+//         if (weight > 0) platesNeeded.push(["custom", Math.round(weight * 100) / 100]);
+//         return platesNeeded;
+// }
 
-/* One-Rep Max Calculator */
+// /* One-Rep Max Calculator */
 
-// Calculate max reps
+// // Calculate max reps
 
-function getMaxReps(weight, reps){
-    let maxReps = [];
-    let oneRepMax = weight * (36 / (37 - reps)); // Brzycki formula
+// function getMaxReps(weight, reps){
+//     let maxReps = [];
+//     let oneRepMax = weight * (36 / (37 - reps)); // Brzycki formula
     
-    for (let i = 1; i <= 10; i++){
-        let estimatedWeight;
-        estimatedWeight = Math.floor((37 - i) * oneRepMax / 36);
-        maxReps.push([estimatedWeight, i])
-    }
-    return maxReps;
-}
+//     for (let i = 1; i <= 10; i++){
+//         let estimatedWeight;
+//         estimatedWeight = Math.floor((37 - i) * oneRepMax / 36);
+//         maxReps.push([estimatedWeight, i])
+//     }
+//     return maxReps;
+// }
 
-// Update visualizer
+// // Update visualizer
 
-function updateRepVizualizer(maxReps) {
-    removeChildNodes(visualizer);
+// function updateRepVizualizer(maxReps) {
+//     removeChildNodes(visualizer);
 
-    // Show 1-rep max
-    let maxHeading = document.createElement("h3");
-    maxHeading.textContent = maxReps[0][1] + " \u00D7 " + maxReps[0][0];
+//     // Show 1-rep max
+//     let maxHeading = document.createElement("h3");
+//     maxHeading.textContent = maxReps[0][1] + " \u00D7 " + maxReps[0][0];
 
-    // Show lists with other rep maxes
-    let maxList = document.createElement("ul");
+//     // Show lists with other rep maxes
+//     let maxList = document.createElement("ul");
     
-    for (let i = 1; i < maxReps.length; i++){
-        let maxListItem = document.createElement("li");
-        maxListItem.textContent = maxReps[i][1] + " \u00D7 " + maxReps[i][0];
-        maxList.appendChild(maxListItem);
-    }
+//     for (let i = 1; i < maxReps.length; i++){
+//         let maxListItem = document.createElement("li");
+//         maxListItem.textContent = maxReps[i][1] + " \u00D7 " + maxReps[i][0];
+//         maxList.appendChild(maxListItem);
+//     }
 
-    visualizer.appendChild(maxHeading)
-    visualizer.appendChild(maxList);
-}
+//     visualizer.appendChild(maxHeading)
+//     visualizer.appendChild(maxList);
+// }
